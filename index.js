994b0b8b92cdd1fe7dbb390a8b0bd95bedb3a520
miya0001 <miya@wpist.me>
2014-09-13 05:07:38 +0900

    navigation action

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Cylon = require('cylon');
 var fly = false;
+var flip = false;
 var calibrated = false;
 
 Cylon.robot({
@@ -17,53 +18,54 @@ Cylon.robot({
     after((1).seconds(), function() {
       console.log("Setting up Collision Detection...");
       my.sphero.detectCollisions();
-      // To detect locator, accelOne and velocity from the sphero
-      // we use setDataStreaming.
-      // sphero API data sources for locator info are as follows:
-      // ['locator', 'accelOne', 'velocity']
-      // It is also possible to pass an opts object to setDataStreaming():
       var opts = {
-        // n: int, divisor of the max sampling rate, 400 hz/s
-        // n = 40 means 400/40 = 10 data samples per second,
-        // n = 200 means 400/200 = 2 data samples per second
         n: 100,
-        // m: int, number of data packets buffered before passing them to the stream
-        // m = 10 means each time you get data it will contain 10 data packets
-        // m = 1 is usually best for real time data readings.
         m: 1,
-        // pcnt: 1 -255, how many packets to send.
-        // pcnt = 0 means unlimited data Streaming
-        // pcnt = 10 means stop after 10 data packets
         pcnt: 0,
       };
-      my.sphero.setDataStreaming(['locator'], opts);
-
-      my.sphero.configureLocator(1, 0, 0, 0);
+      my.sphero.setDataStreaming(['imu'], opts);
       my.sphero.startCalibration();
       my.sphero.stop();
     });
 
     my.sphero.on('data', function(data) {
-      if (!calibrated) {
-          return;
+      if (!fly == true) {
+        return;
       }
-      console.log(data[0]);
-      if (data[0] < 0) {
-        if (!fly) {
-          my.drone.takeoff();
-          fly = true;
-        }
+
+      if (50 > data[2]) {
+        console.log('clockwise');
+        my.drone.clockwise(1);
+      } else if (150 < data[2]) {
+        console.log('counterclockwise');
+        my.drone.counterClockwise(1);
+      } else if (-50 > data[0]) {
+        console.log('front');
+        my.drone.front(0.5);
+      } else if (50 < data[0]) {
+        console.log('back');
+        my.drone.back(0.5);
+      } else if (-50 > data[1]) {
+        console.log('left');
+        my.drone.left(0.5);
+      } else if (50 < data[1]) {
+        console.log('right');
+        my.drone.right(0.5);
       } else {
-        if (fly) {
-          my.drone.land();
-          fly = false;
-        }
-      }
+        console.log('stop');
+        my.drone.stop();      }
     });
 
     my.sphero.on('collision', function(data) {
-      my.sphero.finishCalibration();
-      calibrated = true;
+        if (fly) {
+          console.log('Landing ...');
+          my.drone.land();
+          fly = false;
+        } else {
+          console.log('Take Off ...');
+          my.drone.takeoff();
+          fly = true;
+        }
     });
 
   }
